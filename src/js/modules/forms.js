793a166b1a67212c530a17523b9811de5218760b
AlexandrRepkin <arepkin96@mail.ru>
2023-09-18 18:25:35 +0300

    forms

---
+++ src/js/modules/forms.js
@@ -0,0 +1,120 @@
+export default class Form {
+    constructor(forms) {
+        this.forms = document.querySelectorAll(forms);
+        this.inputs = document.querySelectorAll('input');
+        this.message = {
+            loading: 'Загрузка...',
+            success: 'Спасибо! Скоро мы свяжеися с вами!',
+            failure: 'Что-то пошло не так...'
+        };
+        this.path = 'assets/question.php'
+    }
+
+    clearInputs() {
+        this.inputs.forEach(input => {
+            input.value = '';
+        });
+    }
+
+    checkMaitInputs() {
+        const mailInputs = document.querySelectorAll('[type="email"]');
+
+        mailInputs.forEach(input => {
+            input.addEventListener('keypress', function(e) {
+                if (e.key.mathc(/\[^a-z 0-9 @ \.]/ig)) {
+                    e.preventDefault();
+                } 
+            });
+        });
+    }
+
+    async postData(url, data) {
+        let res = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+
+        return await res.text();
+    }
+
+    initMask() {
+        let setCursorPosition = (pos, elem) => {
+            elem.focus();
+    
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                let range = elem.createTextRange();
+    
+                range.collaps(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        };
+    
+        function createMask(event) {
+            let matrix = '+1 (___) ___-____',
+                i = 0,
+                def = matrix.replace(/\D/g, ''),
+                val = this.value.replace(/\D/g, '');
+    
+            if (def.length >= val.length) {
+                val = def;
+            }
+    
+            this.value = matrix.replace(/./g, function(a) {
+                return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+            });
+    
+            if (event.type === 'blur') {
+                if (this.value.length == 2) {
+                    this.value = '';
+                }
+            } else {
+                setCursorPosition(this.value.length, this);
+            }
+        };
+    
+        let inputs = document.querySelectorAll('[name="phone"]');
+    
+        inputs.forEach(input => {
+            input.addEventListener('input', createMask);
+            input.addEventListener('focus', createMask);
+            input.addEventListener('blur', createMask);
+        });
+    }
+
+    init() {
+        this.checkMaitInputs();
+        this.initMask();
+        
+        this.forms.forEach(form => {
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                let statusMessage = document.createElement('div');
+                statusMessage.style.cssText = `
+                    margin-top: 15px;
+                    font-size: 18px;
+                    color: grey;    
+                `;
+                form.parentNode.appendChild(statusMessage);
+                statusMessage.textContent = this.message.loading;
+
+                const formData = new FormData(form);
+                
+                this.postData(this.path, formData)
+                    .then(res => {
+                        console.log(res);
+                        statusMessage.textContent = this.message.success;
+                    })
+                    .catch(() => statusMessage.textContent = this.message.failure)
+                    .finally(() => {
+                        this.clearInputs();
+                        setTimeout(() => statusMessage.remove(), 6000)
+                    });
+            });
+        });
+    }
+}
